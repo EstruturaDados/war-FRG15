ebe7b3828e0701f2bada41a8299c9d7281842ab9
FRG15 <127434001+FRG15@users.noreply.github.com>
2025-11-25 01:51:51 -0300

    Update projetotema1.c

--- projetotema1.c
+++ projetotema1.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
-struct territorio 
+typedef struct {
+  char [30];
+  cor [10];
+  int tropas;
+} territorio;
+
+int main () {
+  territorio t[5];
+
+printf("Insira um território:");
+
+for (int i = 0; i < 5; i++) {
+printf ("Território: %d", i + 1);
+
+printf ("nome:");
+
+  
