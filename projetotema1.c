d2f323cce23551f3d2ac9bfba9ee4ade706a5eba
FRG15 <127434001+FRG15@users.noreply.github.com>
2025-11-25 21:50:31 -0300

    Update projetotema1.c

--- projetotema1.c
+++ projetotema1.c
@@ -16,5 +16,23 @@ for (int i = 0; i < 5; i++) {
 printf ("Território: %d", i + 1);
 
 printf ("nome:");
+fgets(t[i].nome, 50, stdin);
+t[i].nome[strcspn(t[i].nome,"\n")] = '\0';
+
+printf("cor do exercito:");
+scanf("%d", &t[i].tropas);
+getchar();
+}
+
+printf("MAPA ATUAL");
+for (int i = 0; i < 5; i++) {
+printf("territorio %d", i+1);
+printf("nome: %s", t[i].nome);
+printf("cor: %s", t[i].cor);
+printf("Tropas: %d", t[i].tropas);
+}
+
+return 0; }
+
 
   
