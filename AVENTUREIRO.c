ad7238f369279a50a6c90533483340acab688f94
FRG15 <127434001+FRG15@users.noreply.github.com>
2025-11-25 22:17:32 -0300

    Create AVENTUREIRO.c

---
+++ AVENTUREIRO.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+typedef struct {
+    char [30];
+    cor [10];
+    int tropas;
+} territorio;
+
+void ataque (territorio *t, int at, int def) {
+  int dadocante = rand() % 6 + 1;
+  int dadoensor = rand() % 6 + 1; 
+printf("")
